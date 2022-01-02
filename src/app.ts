1f412a78e157c8c7f3ba3ce49b72b24249e08f44
PrinceNarteh <prinarteh@gmail.com>
2022-01-02 14:01:56 +0000

    fix pino logging issue

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express, { Application } from "express";
 import config from "config";
 import connect from "./utils/connect";
+import logger from "./utils/logger";
 
 const port = config.get<number>("port");
 
 const app: Application = express();
 
-app.listen(port, () => {
-  console.log(`🚀 Server running on http://localhost:${port}`);
-  connect();
+app.listen(port, async () => {
+  logger.info(`Server running on http://localhost:${port}`);
+  await connect();
 });
