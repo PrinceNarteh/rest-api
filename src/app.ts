277bf6d82e0c928f6eb72b1785a863ecf37566c1
PrinceNarteh <prinarteh@gmail.com>
2022-01-02 14:09:59 +0000

    add routes module

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
-import express, { Application } from "express";
+import express, { Express } from "express";
 import config from "config";
 import connect from "./utils/connect";
 import logger from "./utils/logger";
+import routes from "./routes";
 
 const port = config.get<number>("port");
 
-const app: Application = express();
+const app: Express = express();
 
 app.listen(port, async () => {
   logger.info(`Server running on http://localhost:${port}`);
   await connect();
+  routes(app);
 });
